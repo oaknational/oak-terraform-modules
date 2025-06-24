d5fa339f6ec0ac1d108e17148a0183910ee73740
Kofoworola Ogunleye <kay16th@yahoo.com>
2025-06-24 12:35:30 -0400

    fix: cloudflare v5.6.0 expects FQDN
    - It accepts both FQDN and name,
    
    but always want to update in place from owa-vercel.thenational.academy -> owa-vercel
    
    Using the FQDN solves this

--- modules/vercel_project/dns.tf
+++ modules/vercel_project/dns.tf
@@ -8,7 +8,7 @@ resource "cloudflare_dns_record" "this" {
   for_each = { for domain in local.all_domains : domain.name => domain }
 
   zone_id = data.cloudflare_zone.this.zone_id
-  name    = replace(each.value.name, "/\\.?${var.cloudflare_zone_domain}$/", "")
+  name    = each.value.name
   type    = "CNAME"
   content = "cname.vercel-dns.com"
   proxied = true
