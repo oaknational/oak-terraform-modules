69f380710e6f06e91e8b7d92f1003d6c5964668a
Kofoworola Ogunleye <kay16th@yahoo.com>
2026-04-14 05:09:53 -0400

    feat: add sentry project terraform module
    Creates a reusable Sentry project module that creates a Sentry project
    and outputs the DSN for use by other modules.

---
+++ modules/sentry_project/variables.tf
@@ -0,0 +1,25 @@
+variable "platform" {
+  description = "The platform for the Sentry project."
+  type        = string
+
+  validation {
+    condition     = contains(["javascript-nextjs", "node-express"], var.platform)
+    error_message = "Platform must be one of: javascript-nextjs, node-express."
+  }
+}
+
+variable "repo_name" {
+  description = "The name of the Sentry project.This should be the repo name."
+  type        = string
+}
+
+variable "sentry_organization_slug" {
+  description = "The slug of the Sentry organization."
+  type        = string
+}
+
+variable "sentry_team_slug" {
+  description = "The slug of the Sentry team."
+  type        = string
+  default     = "oak-national-academy"
+}
