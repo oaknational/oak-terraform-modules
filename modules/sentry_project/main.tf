9bad84ee7b18e5dfb2bf45a16f5468e4a4f2d898
Amy Matthews <amy.matthews@thenational.academy>
2026-05-13 16:51:15 +0100

    feat: split the sentry name into 2 parts, to allow a suffix

--- modules/sentry_project/main.tf
+++ modules/sentry_project/main.tf
@@ -7,12 +7,16 @@ data "sentry_team" "this" {
   slug         = var.sentry_team_slug
 }
 
+locals {
+  normalized_name = lower(replace("${var.name_parts.git_repo}-${var.name_parts.suffix}", "_", "-"))
+}
+
 resource "sentry_project" "this" {
   organization = data.sentry_organization.this.slug
 
   teams         = [data.sentry_team.this.slug]
-  name          = var.repo_name
-  slug          = var.repo_name
+  name          = local.normalized_name
+  slug          = local.normalized_name
   platform      = var.platform
   default_rules = false
 }
